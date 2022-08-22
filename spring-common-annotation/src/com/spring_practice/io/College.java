6a996e24b32c39de2808743e5679ea323d672a72
Arunkaash <arunkumarkr101@gmail.com>
2022-08-22 12:23:02 +0530

    first commit

---
+++ spring-common-annotation/src/com/spring_practice/io/College.java
@@ -0,0 +1,8 @@
+package com.spring_practice.io;
+
+import org.springframework.stereotype.Component;
+
+@Component("collegeBean")
+public class College {
+
+}
---
+++ spring-common-annotation/src/com/spring_practice/io/CollegeConfig.java
@@ -0,0 +1,10 @@
+package com.spring_practice.io;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="com.spring_practice.io")
+public class CollegeConfig {
+
+}
